96fbe0df1377bbdd2ebb5c3a859a0c043415ddbb
OM-PC\OM <the.fake.co.in@gmail.com>
2021-02-14 15:19:44 +0530

    First Commit: Add all Files to GitHub. Login Page Done. Dashboard Started.

---
+++ LMS/Content/Login/Login.js
@@ -0,0 +1,113 @@
+﻿$(document).ready(function () {
+
+    //Disable submit form by Enter Key Press
+    $(window).keydown(function (event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            return false;
+        }
+    });
+
+    $('#show-modal').click(function () {
+        $("#login-modal").modal('show')
+        $('#error-msg').html('')
+        GoToLogin();
+    });
+
+    function CloseModal() {
+        $("#login-modal").modal('hide')
+        $('#error-msg').html('')
+    }
+
+    $('#close-modal').click(function () {
+        CloseModal();
+    });
+
+    $('#close-modal-2').click(function () {
+        CloseModal();
+    });
+
+    $('#close-modal-3').click(function () {
+        CloseModal();
+    });
+
+
+    $('#forgot-pwd').click(function () {
+        $('#div-login').hide('slow')
+        $('#div-forgot-pwd').show('slow')
+        $('#div-set-pwd').hide('slow')
+    });
+
+    $('#goto-login').click(function () {
+        GoToLogin();
+    });
+
+    $('#goto-login-2').click(function () {
+        GoToLogin();
+    });
+
+    function GoToLogin() {
+        $('#div-login').show('slow')
+        $('#div-forgot-pwd').hide('slow')
+        $('#div-set-pwd').hide('slow')
+    }
+
+    // Validate form for Empty/invalid Fields before submit
+    function validateForm() {
+        var errorMsg = "";
+        if ($(username).val().trim().length == 0) {
+            errorMsg = 'Please enter UserName...</br>';
+            $('#div-username').tooltip('show')
+        }
+        if ($(password).val().trim().length == 0) {
+            errorMsg += 'Please enter Password...</br>';
+            $('#div-password').tooltip('show')
+        }
+        return errorMsg;
+    };
+
+    $('#Login').click(function () {
+        var username = $('#username').val();
+        var password = $('#password').val();
+
+        debugger;
+        var errorMsg = validateForm();
+        if (errorMsg == "") {
+            $.ajax({
+                url: "Home/VerifyLogin",
+                method: "Get",
+                data: { username: username, password: password },
+                cache: false,
+                beforeSend: function () {
+                    debugger;
+                    $('#Login').val("Connecting...");
+                },
+                success: function (data) {
+                    $('#Login').val("Login");
+                    debugger;
+                    if (data == "Success") {
+                        // similar behavior as an HTTP redirect
+                        window.location.replace("Home/LMSDashboard");
+
+                        // similar behavior as clicking on a link
+                        //window.location.href = "Home/LMSDashBoard";
+                    }
+                    else {
+                        var options = {
+                            distance: '40',
+                            direction: 'left',
+                            times: '3'
+                        }
+                        debugger;
+                        $('#error-msg').html(data);
+                    }
+                }
+            });
+        }
+        else {
+            debugger;
+            //$('#error-msg').html(errorMsg);
+        }
+    });
+
+}); 
---
+++ LMS/Controllers/LoginController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Login(UserMaster objUser)
+        //{
+        //    if (true || ModelState.IsValid)
+        //    {
+        //        using (LMSEntities db = new LMSEntities())
+        //        {
+        //            var obj = db.UserMasters.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+        //            if (obj != null)
+        //            {
+        //                Session["UserID"] = obj.UserId.ToString();
+        //                Session["UserName"] = obj.UserName.ToString();
+        //                return RedirectToAction("LMSDashboard");
+        //            }
+        //            else
+        //            {
+        //                this.ModelState.AddModelError("AuthenticationFailed", "Enter valid UserName & password!");
+        //                return View("Login");
+        //            }
+        //        }
+        //    }
+        //    return View(objUser);
+        //}
+
+        [HttpGet]
+        public string VerifyLogin(string username, string password)
+        {
+            if (true || ModelState.IsValid)
+            {
+                using (LMSEntities db = new LMSEntities())
+                {
+                    var obj = db.UserMasters.Where(a => a.UserName.Equals(username) && a.Password.Equals(password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserId.ToString();
+                        Session["UserName"] = obj.UserName.ToString();
+                        return "Success";
+                    }
+                    else
+                    {
+                        this.ModelState.AddModelError("AuthenticationFailed", "Enter valid UserName & password!");
+                        return "Enter valid UserName & password!";
+                    }
+                }
+            }
+            return "Failed!";
+        }
+
+        [HttpGet]
+        public ActionResult LMSDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Dashboard");
+            }
+        }
+    }
+}
